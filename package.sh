b399e2dc50a7d2111a05c50273c1531325c4b09c
MACHANDRA11 <91306369+MACHANDRA11@users.noreply.github.com>
2021-11-04 21:58:34 +0530

    added

--- package.sh
+++ package.sh
@@ -1,3 +1,3 @@
 mkdir -p ~/bin
-curl -L https://github.com/GoogleContainerTools/kpt/releases/download/v1.0.0-beta.1/kpt_linux_amd64 --output ~/bin/kpt && chmod u+x ~/bin/kpt
+curl -L https://github.com/GoogleContainerTools/kpt/releases/download/v0.39.2/kpt_linux_amd64 --output ~/bin/kpt && chmod u+x ~/bin/kpt
 export PATH=${HOME}/bin:${PATH}
